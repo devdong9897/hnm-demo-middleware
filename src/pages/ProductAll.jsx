f8ae5fc3c72149928ada0dd42ef6bd66ad97df14
devdong9897 <dev.dong9897@gmail.com>
2026-02-24 11:34:49 +0900

    feat: 미들웨어로 데이터 가져오기

--- src/pages/ProductAll.jsx
+++ src/pages/ProductAll.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ProductCard from "../components/ProductCard";
 import { Container, Row, Col } from "react-bootstrap";
 import { useSearchParams } from "react-router-dom";
 import { productAction } from "../redux/actions/productAction";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const ProductAll = () => {
-  const [productList, setProductList] = useState([]);
+  const productList = useSelector((state) => state.productList);
   const [query] = useSearchParams();
   const dispatch = useDispatch();
 
@@ -14,7 +14,7 @@ const ProductAll = () => {
     let searchQuery = query.get("q") || "";
     console.log("쿼리값은", searchQuery);
     // 여기서 {type: ""}이라고 하면 바로 store로 가기 때문에 아래처럼 작성하면 미들웨어로 거쳐서 간다.
-    dispatch(productAction.getProducts());
+    dispatch(productAction.getProducts(searchQuery));
   };
 
   useEffect(() => {
--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -1,12 +1,12 @@
-function getProducts() {
+function getProducts(searchQuery) {
   // getState: 현재의 state정보
   return async (dispatch, getState) => {
     let url = `http://localhost:4000/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
-    setProductList(data);
+    // 여기서 이제 reducer로 보냄
+    dispatch({ type: "GET_PRODUCT_SUCCESS", payload: { data } });
   };
 }
 
-export productAction = { getProducts }
-
+export const productAction = { getProducts };
